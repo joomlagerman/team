1060a9f83da5c9aae4c4e66ec13bdf481f54fe0a
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2019-04-24 22:21:16 +0200

    remove cod and Licence from download

--- Hilfsprogramme/Release/createTranslationRelease/helpers/general.sh
+++ Hilfsprogramme/Release/createTranslationRelease/helpers/general.sh
@@ -41,7 +41,7 @@ function loadGit()
 
     echo -e "- Lösche .git-Dateien... \c"
     find "${GIT_DIR}" -type d -name '.git' | xargs rm -rf
-    rm -f "${GIT_DIR}/.gitattributes" "${GIT_DIR}/README.md" "${GIT_DIR}/.gitignore"
+    rm -f "${GIT_DIR}/.gitattributes" "${GIT_DIR}/.gitignore" "${GIT_DIR}/CODE_OF_CONDUCT.md" "${GIT_DIR}/README.md" "${GIT_DIR}/LICENCE"
     echo -e "OK!\n"
 }
 
